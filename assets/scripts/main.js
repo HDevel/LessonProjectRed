241a13693fbd8f9ebcf99aecb7360d7f2f77ff5c
misha <H.Devel@gmail.com>
2015-04-03 21:47:32 +0300

    Восстановка

---
+++ assets/scripts/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    //$('#modules > div:before').each(function (id, val) {
+    //    val.onclick = function () {
+    //        scaleModule(val, ['wSmall', 'wMedium', 'wLarge']);
+    //    }
+    //});
+    $('#modules > div').each(function (id, val) {
+        var wClasses = [{c:'wSmall',t:'S'}, {c:'wMedium',t:'M'}, {c:'wLarge',t:'L'}];
+        var hClasses = [{c:'hSmall',t:'S'}, {c:'hMedium',t:'M'}, {c:'hLarge',t:'L'}];
+
+        var before = document.createElement('b');
+        $(before).addClass('moduleBefore').click(function () {
+            scaleModule(val, before, wClasses, 1);
+        });
+        var after = document.createElement('b');
+        $(after).addClass('moduleAfter').click(function () {
+            scaleModule(val, after, hClasses, 1);
+        });
+        $(val).append(before, after);
+        scaleModule(val, before, wClasses, 0);
+        scaleModule(val, after, hClasses, 0);
+    });
+    function scaleModule(dom, btn, classes, scale) {
+        var mClass = 0;
+        classes.forEach(function (value, index) {
+            if($(dom).hasClass(value.c)){
+                mClass = index + scale;
+            }
+            $(dom).removeClass(value.c);
+        });
+        if(mClass >= classes.length){
+            mClass = 0;
+        }
+        $(dom).addClass(classes[mClass].c);
+        $(btn).html(classes[mClass].t);
+    }
+})
