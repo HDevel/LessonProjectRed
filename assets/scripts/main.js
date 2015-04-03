b9c83add591530a82aa5f96e6468dba28d388f64
Миша <H.Devel@gmail.com>
2015-04-03 23:20:49 +0300

    Поправил ошибки написания по правилам Closure Linter

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,18 +1,26 @@
-$(document).ready(function () {
+$(document).ready(function() {
     addResizeLabel();
 });
 
-function addResizeLabel(){
-    $('#modules > div').each(function (id, val) {
-        var wClasses = [{c:'wSmall',t:'S'}, {c:'wMedium',t:'M'}, {c:'wLarge',t:'L'}];
-        var hClasses = [{c:'hSmall',t:'S'}, {c:'hMedium',t:'M'}, {c:'hLarge',t:'L'}];
+function addResizeLabel() {
+    $('#modules > div').each(function(id, val) {
+        var wClasses = [
+            {c: 'wSmall', t: 'S'},
+            {c: 'wMedium', t: 'M'},
+            {c: 'wLarge', t: 'L'}
+        ];
+        var hClasses = [
+            {c: 'hSmall', t: 'S'},
+            {c: 'hMedium', t: 'M'},
+            {c: 'hLarge', t: 'L'}
+        ];
 
         var before = document.createElement('b');
-        $(before).addClass('moduleBefore').click(function () {
+        $(before).addClass('moduleBefore').click(function() {
             scaleModule(val, before, wClasses, 1);
         });
         var after = document.createElement('b');
-        $(after).addClass('moduleAfter').click(function () {
+        $(after).addClass('moduleAfter').click(function() {
             scaleModule(val, after, hClasses, 1);
         });
         $(val).append(before, after);
@@ -21,13 +29,13 @@ function addResizeLabel(){
     });
     function scaleModule(dom, btn, classes, scale) {
         var mClass = 0;
-        classes.forEach(function (value, index) {
-            if($(dom).hasClass(value.c)){
+        classes.forEach(function(value, index) {
+            if ($(dom).hasClass(value.c)) {
                 mClass = index + scale;
             }
             $(dom).removeClass(value.c);
         });
-        if(mClass >= classes.length){
+        if (mClass >= classes.length) {
             mClass = 0;
         }
         $(dom).addClass(classes[mClass].c);
